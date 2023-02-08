3f02aa7ca899ceac04627b0adf9127e088214f00
Christopher Hill <cjameshill@gmail.com>
2023-02-08 13:19:32 +0100

    Updated session function to set custom header

--- netlify/edge-functions/session.js
+++ netlify/edge-functions/session.js
@@ -2,13 +2,9 @@
 export default async (request, context) => {
     const url = new URL(request.url);
 
-    if (url.searchParams.get("method") !== "set-response-header") {
-        return context.next();
-    }
-
-    console.log(`Adding a custom header to the response for ${url}`);
+    console.log(`Adding a custom cookie to the response for ${url}`);
 
     const response = await context.next();
-    response.headers.set("X-Your-Custom-Header", "Your custom header value");
+    response.cookies.set({ name: "cp-session", value: "cp-session-id" })
     return response;
 };

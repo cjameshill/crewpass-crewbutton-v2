6c525b2e2dd7b9cdad660712c058bba30846a610
Christopher Hill <cjameshill@gmail.com>
2023-02-08 13:37:05 +0100

    Removed set session fetch function

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,5 @@
 <script setup>
 import PrimaryButton from "@/components/PrimaryButton.vue";
-import { onMounted } from "vue";
-const setSession = async () => {
-    const res = await fetch(
-        "https://crewpass-crewbutton-v2-dev.netlify.app/session"
-    );
-    console.log("response from edge function: ", res.text());
-};
-onMounted(async () => {
-    await setSession();
-});
 </script>
 
 <template>

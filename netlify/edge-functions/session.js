6099601787e01e4c2b04a953ffa6b2644dd0d7c3
Christopher Hill <cjameshill@gmail.com>
2023-02-08 14:59:15 +0100

    Added uuid to generate session id

--- netlify/edge-functions/session.js
+++ netlify/edge-functions/session.js
@@ -1,10 +1,10 @@
-
+import { v4 } from "https://deno.land/std@0.136.0/uuid/mod.ts";
 export default async (request, context) => {
     const url = new URL(request.url);
     console.log(`Adding a custom cookie to the response for ${url}`);
     const session = context.cookies.get("cp-session");
     if (!session) {
-        const id = Crypto.randomUUID();
+        const id = v4();
         context.cookies.set({ name: "cp-session", value: id })
     }
     return await context.next();
